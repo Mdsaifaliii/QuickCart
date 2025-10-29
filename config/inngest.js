999f42e06659b5c16b5acf0f523a234100b8ac08
Mdsaifaliii <saifali79873@gmail.com>
2025-10-29 21:18:43 +0530

    https://github.com/Mdsaifaliii/QuickCart

--- config/inngest.js
+++ config/inngest.js
@@ -61,7 +61,7 @@ export const createUserOrder = inngest.createFunction(
   {
     id: 'create-user-order',
     batchEvents: {
-      maxSize: 5,
+      maxSize: 4,
       timeout: '5s'
     }
   },
